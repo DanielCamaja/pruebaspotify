4360755f34eb667678d77f302c22bd016ec6509a
DanielCamaja <dcamajaig@gmail.com>
2018-11-29 23:53:22 -0600

    cambios

---
+++ app/src/main/java/spotifyprueba/com/spotifyprueba/Echo.java
@@ -0,0 +1,33 @@
+package spotifyprueba.com.spotifyprueba;
+
+
+import com.spotify.protocol.types.Item;
+
+public interface Echo {
+
+    class Request implements Item {
+        public final String request;
+
+        public Request(String request) {
+            this.request = request;
+        }
+
+        /* For GSON */
+        public Request() {
+            request = null;
+        }
+    }
+
+    class Response implements Item {
+        public final String response;
+
+        public Response(String response) {
+            this.response = response;
+        }
+
+        /* For GSON */
+        public Response() {
+            response = null;
+        }
+    }
+}
